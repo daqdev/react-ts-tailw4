4317d630818a62304a35eaa8a029fcbb80b11213
u92156 <daquiroga@proveedor.teco.com.ar>
2025-07-28 10:46:55 -0300

    inicial_react_ts_tw4

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  return (
+    <main className="grid h-screen place-items-center bg-slate-900 text-white">
+      <h1 className="text-6xl font-bold text-sky-600">
+        React + TS + Tailwind v4 🎉
+      </h1>
+    </main>
+  );
+}
+export default App;
